55f156d424b052173f374431e185e205ca8a1bf0
tumentafavour <tumentafavour6@gmail.com>
2025-10-17 00:17:20 +0100

    feat: create navbar section

---
+++ Just-hair/src/assets/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Navbar.css';
+import { Bell, User } from 'lucide-react';
+
+
+const Navbar = () => {
+return (
+<nav className="navbar">
+<div className="logo">JH</div>
+<h2 className="welcome">Hi, Jack! Welcome Back</h2>
+<div className="search-section">
+<input type="text" placeholder="Search" className="search-bar" />
+<Bell className="icon" />
+<User className="icon" />
+</div>
+</nav>
+);
+};
+
+
+export default Navbar;
