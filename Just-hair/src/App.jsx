6d04e79fbe47cbbba9eebf18aaec7a6596f26127
chida_nzemssing_ariel <chidanzemssing123@gmail.com>
2025-10-21 18:23:06 +0100

    just hair project

--- Just-hair/src/App.jsx
+++ Just-hair/src/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import Navbar from './components/Navbar'
-import Hero from './components/Hero';
-import Product from './components/Product';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Product from "./components/Product";
+import Book from "./components/Chida/Book";
+import Success from "./components/Chida/Success";
+import Error_one from "./components/Chida/Error_one";
+import Error_two from "./components/Chida/Error_two";
 
 function App() {
-return (
-<>
-<Navbar />
-<Hero/>
-<div style={{ padding: '2rem' }}>
-
-</div>
-</>
-);
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Book />
+      <Success />
+      <Error_one/>
+      <Error_two/>
+      <div style={{ padding: "2rem" }}></div>
+    </>
+  );
 }
 export default App;
