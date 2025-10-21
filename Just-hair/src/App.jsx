c9145724593ae6180c6441a04a31aa8ca1654f6e
marivonashienji@gmail.com <marivon-ashie>
2025-10-21 18:29:12 +0100

    i worked on the sign in page building forget password  page . firstly i created a section for the page then proceeded to  gave a container  for the page ,added a form  that is the email and user name gave a placeholder for the logo  also created a link to the next page i will be working on . proceeded with the styling to for beauty.

--- Just-hair/src/App.jsx
+++ Just-hair/src/App.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import Navbar from './components/Navbar'
-import Hero from './components/Hero';
-import Product from './components/Product';
-
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Product from "./components/Product";
+import SignInPage from "./components/sign_in_page";
 function App() {
-return (
-<>
-<Navbar />
-<Hero/>
-<div style={{ padding: '2rem' }}>
-
-</div>
-</>
-);
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <SignInPage />
+      <div style={{ padding: "2rem" }}></div>
+    </>
+  );
 }
 export default App;
